7801947aa23b28b35cb3e18f578eb55472809744
JoseENarvaezM <josenarvaezm2003@gmail.com>
2024-04-09 22:22:44 -0500

    Main Page 2

--- Frontend/coperativaFrontend/src/pages/MainPage.jsx
+++ Frontend/coperativaFrontend/src/pages/MainPage.jsx
@@ -4,21 +4,15 @@ import '../styles/MainPage.css'
 
 function MainPage() {
     return (
-<<<<<<< HEAD
         <div  className='contenedor'>
                 <img className="logo" src="https://fernandocolmenares.co/wp-content/uploads/2020/08/U.CooperativaCol.png" alt="Logo" />
                 <div className='contenedorIB'>
                     <input className= "campo" type="text" placeholder="Introduzca el código" />
                     <button className='boton'>Enviar</button>
                 </div>
-                <h3 className='texto'>Si no eres estudiante, dale click <a className='link' href='*'>aqui</a> para crear un codigo</h3>
+                <h3 className='texto'>Si no eres estudiante, dale click <a className='link' href=''>aqui</a> para crear un codigo</h3>
                 
         </div>
-=======
-        <>
-            <h1>Holaa</h1>
-        </>
->>>>>>> 51e875b253e0ea77d4be732ab6670941257df2ae
     );
 }
 
