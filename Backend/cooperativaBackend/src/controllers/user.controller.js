aa8323a95ba827286a0d9239bc52dcac1b31db68
Jaycom17 <juanorejuela499@gmail.com>
2024-04-12 22:20:38 -0500

    Activos fijos form was started

--- Backend/cooperativaBackend/src/controllers/user.controller.js
+++ Backend/cooperativaBackend/src/controllers/user.controller.js
@@ -1,8 +1,7 @@
 import { createUser } from "../services/user.service.js";
 
 export const postUser = async (req, res) => {
-    const { usuName, usuEmail, usuPassword, usuRole } = req.body;
-    const newUser = { usuName, usuEmail, usuPassword, usuRole };
+    const newUser = req.body;
 
     const result = await createUser(newUser);
 
--- Backend/cooperativaBackend/src/index.js
+++ Backend/cooperativaBackend/src/index.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import userRouter from './routes/user.routes.js';
+import activosFijosRouter from './routes/activosFijos.routes.js';
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/user', userRouter);
+app.use('/activosFijos', activosFijosRouter);
 
 app.listen(3000, () => {
     console.log('Server started on http://localhost:3000');
