28d5f73aee4d498950c4c5f929800401960802aa
JoseENarvaezM <josenarvaezm2003@gmail.com>
2024-04-14 17:24:00 -0500

    Actualizacion 3

--- Frontend/coperativaFrontend/src/App.jsx
+++ Frontend/coperativaFrontend/src/App.jsx
@@ -1,10 +1,12 @@
 import MainPage from './pages/MainPage'
 import LoginPage from './pages/LoginPage'
 import ItemLista from './components/ItemLista'
+import ListaItems from './pages/ListaItems'
+import Navbar from './components/Navbar'
 function App() {
 
   return (
-    <LoginPage />
+    <ListaItems />
   )
 }
 
---
+++ Frontend/coperativaFrontend/src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import '../styles/Navbar.css';
+
+const Navbar = ({ onCerrarSesion, onCrearProfesor, onActualizarDatos,onLista }) => {
+  return (
+    
+    <nav className="navbar">
+      <div className="navbar-brand">Administador</div>
+      <div className="navbar-actions">
+      <button onLista={onLista} className="btn-lista">
+          Lista de profesores
+        </button>
+        <button onClick={onCrearProfesor} className="btn-crear-profesor">
+          Crear Profesor
+        </button>
+        <button onClick={onActualizarDatos} className="btn-actualizar-datos">
+          Actualizar Datos
+        </button>
+        <button onClick={onCerrarSesion} className="btn-cerrar-sesion">
+          Cerrar Sesión
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
