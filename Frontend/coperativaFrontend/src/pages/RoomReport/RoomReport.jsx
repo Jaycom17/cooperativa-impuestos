7a5e2c7890fbbd32b85a9472ee609866c711e4d4
Jaycom17 <juanorejuela499@gmail.com>
2024-07-09 18:37:10 -0500

    soluciones de errores

--- Frontend/coperativaFrontend/src/pages/RoomReport/RoomReport.jsx
+++ Frontend/coperativaFrontend/src/pages/RoomReport/RoomReport.jsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react";
 import TeacherNavbar from "../../components/TeacherNavBar/TeacherNavbar";
-import { useParams, Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { getStudentsByRoom } from "../../services/student.service";
 
 function RoomReport() {
   const { roomID } = useParams();
   const [students, setStudents] = useState([]);
+  const [selectedStudent, setSelectedStudent] = useState(null);
+  const [studentsCopy, setStudentsCopy] = useState([]);
 
   useEffect(() => {
     const fetchStudents = async () => {
@@ -15,43 +17,38 @@ function RoomReport() {
           a.stuName.localeCompare(b.stuName)
         );
         setStudents(sortedData);
+        setStudentsCopy(sortedData);
       } catch (error) {
         console.error("Error al obtener los estudiantes de la sala:", error);
       }
     };
     fetchStudents();
   }, [roomID]);
+
+  const searchStudent = (e) => {
+    const searchValue = e.target.value;
+    const filteredStudents = studentsCopy.filter((student) =>
+      student.stuName.toLowerCase().includes(searchValue.toLowerCase())
+    );
+    setStudents(filteredStudents);
+  }
+
   return (
     <>
       <TeacherNavbar />
       <main className="flex">
-        <aside className="w-32 h-screen bg-buttons-login flex flex-col gap-1">
-          <input type="text" className="w-full" />
+        <aside className="w-32 h-screen bg-buttons-login flex flex-col gap-1 lg:w-40">
+          <div className="w-full p-1">
+            <input onChange={searchStudent} placeholder="Busca estudiante" type="text" className="w-full rounded p-1" />
+          </div>
           {students.map((student) => (
-            <div className="w-full" key={student.stuID}>
-              <Link
-                className="text-center text-xl"
-                to={`/roomstudentreport/${roomID}/${student.stuID}`}
-              >
-                <h1 className="hover:underline hover:bg-[#808080]">
-                  {student.stuName}
-                </h1>
-              </Link>
-            </div>
+              <button key={student.stuID}
+                onClick={() => setSelectedStudent(student)}
+                className={`hover:bg-[#808080] w-full text-white ${selectedStudent?.stuID === student.stuID ? "bg-[#808080]" : "bg-buttons-login"}`}
+              >{student.stuName}</button>
           ))}
         </aside>
       </main>
-      {/**
-     * <section className="p-4 flex gap-1 flex-col w-full items-start">
-    {students.map((student) => (
-        <div className="bg-red-200 w-1/6" key={student.stuID} >
-          <Link className="text-center text-xl hover:underline" to={`/roomstudentreport/${roomID}/${student.stuID}`}>
-            <h1>{student.stuName}</h1>
-        </Link>
-        </div>
-    ))}
-    </section>
-     */}
     </>
   );
 }
