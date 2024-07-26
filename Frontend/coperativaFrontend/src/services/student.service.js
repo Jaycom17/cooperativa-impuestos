978c1f3b93ef1ae4b1c929965e68993db52c8eab
Jaycom17 <juanorejuela499@gmail.com>
2024-07-26 18:30:41 -0500

    some mistakes were fixed

--- Frontend/coperativaFrontend/src/services/student.service.js
+++ Frontend/coperativaFrontend/src/services/student.service.js
@@ -2,6 +2,6 @@ import axios from './axios.service.js'
 
 export const createStudent = async (student) => axios.post("http://localhost:3000/student", student);
 
-export const searchStudent = async (stuName, roomID) => axios.get(`http://localhost:3000/student/search/${stuName}/${roomID}`);
+export const searchStudent = async (stuCedula, roomID) => axios.get(`http://localhost:3000/student/search/${stuCedula}/${roomID}`);
 
 export const getStudentsByRoom = async (roomID) => axios.get(`http://localhost:3000/student/room/${roomID}`);
